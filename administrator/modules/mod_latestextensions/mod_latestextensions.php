9390d88be78c3a3a7bb09b0a64ba079472a4c5de
prakash-a7x <prakash.a7x@gmail.com>
2022-01-06 17:30:47 +0600

    class name changed to capital

--- administrator/modules/mod_latestextensions/mod_latestextensions.php
+++ administrator/modules/mod_latestextensions/mod_latestextensions.php
@@ -5,6 +5,6 @@ use Joomla\CMS\Helper\ModuleHelper;
 
 require_once __DIR__ . '/helper.php';
 
-$list = mod_latestextensionsHelper::getList($params);
+$list = Mod_latestextensionsHelper::getList($params);
 $moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
 require ModuleHelper::getLayoutPath('mod_latestextensions', $params->get('layout', 'default'));
